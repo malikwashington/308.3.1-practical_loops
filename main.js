a1dc1112797711579f7ed863ff77897a83401716
malikwashington <112908272+malikwashington@users.noreply.github.com>
2024-09-23 10:51:48 -0400

    I limited nextPrime() so that the logic could be simpler. I also made it so that parseCSV() will output to a table if you run main.js

--- main.js
+++ main.js
@@ -25,3 +25,22 @@ const nextPrime = () => {
 }
 
 console.log(nextPrime())
+
+
+// 4 cells per row, \n escape char print all the cells
+//
+//
+
+const exampleString = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
+
+
+const parseCSV=(str, esc='\r\n')=>{
+        let s = str.split(esc)
+        const key = s.shift().split(',')
+        for(const [i,e] of s.entries())
+                s[i]=Object.fromEntries(e.split(',').slice(1).map((e,i)=>[key[i],e]))
+        return s
+}
+
+const res = parseCSV(exampleString, '\n')
+console.table(res)
